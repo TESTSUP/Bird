c100ab6d0135cb007c1a2e96acf5ae8ed06ba07c
sunyonggang <yonggang.sun@chukong-inc.com>
2015-06-18 23:30:04 +0800

    添加分类tableView

--- Bird/Bird/HomeViewController.m
+++ Bird/Bird/HomeViewController.m
@@ -9,7 +9,7 @@
 #import "HomeViewController.h"
 #import "CategoryViewController.h"
 
-@interface HomeViewController ()
+@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate>
 {
     UITableView *_categoryTableView;
 }
@@ -20,7 +20,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createTableView];
+}
 
+- (void)createTableView
+{
+    _categoryTableView = [[UITableView alloc] initWithFrame:CGRectZero
+                                                      style:UITableViewStylePlain];
+    _categoryTableView.delegate = self;
+    _categoryTableView.dataSource = self;
+    _categoryTableView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:_categoryTableView];
+    
+    [_categoryTableView makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view);
+        make.bottom.equalTo(self.view);
+        make.left.equalTo(self.view);
+        make.width.equalTo(@75);
+    }];
+    
+    
 }
 
 - (NSString *)title
@@ -31,6 +50,37 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma makr - UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+#pragma makr - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"categoryCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellId];
+    }
+    
+    return cell;
 }
 
 @end
